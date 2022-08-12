94ded114df8f1f171f897fc73f3f2e9751e117ae
Suraj M Durgad <suraj@quizizz.com>
2022-08-12 09:33:48 +0530

    Bug fix - https://github.com/joltup/rn-fetch-blob/issues/490\#issuecomment-990899440

--- android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
+++ android/src/main/java/com/RNFetchBlob/RNFetchBlobReq.java
@@ -603,7 +603,7 @@ public class RNFetchBlobReq extends BroadcastReceiver implements Runnable {
                 RNFetchBlobFileResp rnFetchBlobFileResp;
 
                 try {
-                    rnFetchBlobFileResp = (RNFetchBlobFileResp) responseBody;
+                    rnFetchBlobFileResp = new RNFetchBlobFileResp( responseBody );
                 } catch (ClassCastException ex) {
                     // unexpected response type
                     if (responseBody != null) {
